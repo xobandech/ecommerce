3862ea6d6014421f955a825aebe1f22f4a7ad569
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-06-20 09:15:46 +0200

    Remake HomePage to .tsx

---
+++ src/components/Home/HomePage.tsx
@@ -0,0 +1,11 @@
+import News from "../News/News";
+import React from "react";
+const HomePage = () => {
+  return (
+    <div className="home-page">
+        <News />
+    </div>
+  );
+};
+
+export default HomePage;
