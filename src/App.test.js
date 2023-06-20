6c2c557665cea08899bca2841e089440d14fa82a
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-06-20 09:09:15 +0200

    Write some tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import NavigationBar from './components/NavBar/NavigationBar';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/News/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('renders navbar', () => {
+  render(<App />);
+  
+  const homeLink = screen.getByText(/Home/i);
+  const cartLink = screen.getByText(/Cart/i);
+  const productsLink = screen.getByText(/Products/i);
+  expect(cartLink).toBeInTheDocument();
+  expect(homeLink).toBeInTheDocument();
+  expect(productsLink).toBeInTheDocument();
+})
